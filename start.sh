b0cfaec1bdfe54bd8fede5b671f9d90bf1f6548a
BELUGA_XD <96910602+belugaop@users.noreply.github.com>
2022-09-22 21:04:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kkhanyaseen/autofilter.git /autofilter
+  git clone https://github.com/belugaop/jack-auto-filter-bot.git /jack-auto-filter-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /autofilter
+  git clone $UPSTREAM_REPO /jack-auto-filter-bot
 fi
 cd /autofilter
 pip3 install -U -r requirements.txt
